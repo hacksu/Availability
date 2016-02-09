de64c0098ef94b917d998438f72d10488d0002a3
Isaac Park <isaac.a.park@gmail.com>
2016-02-09 12:41:50 -0500

    Protect Server

--- AvailabilityBackend.py
+++ AvailabilityBackend.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
-
+import re
 availability = {}
 
 app = Flask(__name__)
@@ -21,7 +21,25 @@ def get_availability():
 def record_availability():
     global availability
 
-    availability.update(request.get_json())
+    json = request.get_json()
+    if not json:
+        return jsonify({"error": "not JSON"})
+
+    if len(json) != 1:
+        return jsonify({"error": "wrong len"})
+
+    safe = {}
+    for key in json:
+        value = json[key]
+        print(value, key)
+        key = re.sub('[<>*&#@$;:]', '', key)
+
+        value = re.sub('[<>*&#@;:]', '', value)
+        print(value, key)
+        if value == "Free" or value == "Busy" or value == "Putting out a fire" or value == "Running really fast":
+            safe[key] = value
+
+    availability.update(safe)
     return jsonify(availability)
 
 if __name__ == "__main__":
